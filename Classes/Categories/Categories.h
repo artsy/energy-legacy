9834e5a66c91f1681d70369550e55d96b2d961d9
Orta Therox <orta.therox@gmail.com>
2015-08-12 10:30:33 -0400

    Support running from the OSS fonts

--- Classes/Categories/Categories.h
+++ Classes/Categories/Categories.h
@@ -2,7 +2,15 @@
 #import "NSString+StringBetweenStrings.h"
 #import <Artsy+UIColors/UIColor+ArtsyColors.h>
 #import "UIColor+FolioColours.h"
+
+#if __has_include(<Artsy+UIFonts/UIFont+ArtsyFonts.h>)
 #import <Artsy+UIFonts/UIFont+ArtsyFonts.h>
+#endif
+
+#if __has_include(<Artsy+OSSUIFonts/UIFont+OSSArtsyFonts.h>)
+#import <Artsy+OSSUIFonts/UIFont+OSSArtsyFonts.h>
+#endif
+
 #import "NSObject+Notifications.h"
 #import <UIView+BooleanAnimations/UIView+BooleanAnimations.h>
 #import "UIButton+FolioButtons.h"
