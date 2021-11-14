f02c7850654a5d617a0e04fa9fd5b510676facbd
Mounir Dhahri <soussouheros@gmail.com>
2021-11-14 17:30:25 +0100

    add fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react"
-import { Text } from "react-native"
-import { Theme, Flex } from "palette"
+import { Theme, Flex, Text } from "palette"
 
 const App = () => {
   return (
     <Theme>
       <Flex flex={1} justifyContent="center" alignItems="center">
-        <Text>Works</Text>
+        <Text variant="md">Fonts work!</Text>
       </Flex>
     </Theme>
   )
