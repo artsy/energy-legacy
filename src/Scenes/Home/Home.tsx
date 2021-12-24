0d810896cb6b49cc380c14eb151a591cb7bfc991
Mounir Dhahri <soussouheros@gmail.com>
2021-12-24 18:24:10 +0100

    separate screens

---
+++ src/Scenes/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { Flex, Button } from "palette"
+import { NativeStackScreenProps } from "@react-navigation/native-stack"
+import { MainNavigationStack } from "MainNavigationStack"
+
+interface HomeNavigationProps extends NativeStackScreenProps<MainNavigationStack, "Home"> {}
+
+export const HomeScreen: React.FC<HomeNavigationProps> = ({ navigation }) => (
+  <Flex flex={1} justifyContent="center" alignItems="center">
+    <Button onPress={() => navigation.goBack()}>Navigate back to Login Screen</Button>
+  </Flex>
+)
