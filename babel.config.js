87faa473ba6d5948afbd35a256d5ad8b0ec930ac
Mounir Dhahri <soussouheros@gmail.com>
2022-01-03 17:47:30 +0100

    remove console.logs from ci builds (#12)

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,17 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [
-    // the relay plugin should run before other plugins or presets
-    // to ensure the graphql template literals are correctly transformed
-    "relay",
-  ],
+const presets = [
+  "module:metro-react-native-babel-preset"
+]
+
+const plugins = [
+  // the relay plugin should run before other plugins or presets
+  // to ensure the graphql template literals are correctly transformed
+  "relay",
+]
+
+if (process.env.CI) {
+  // When running a bundled app, these statements can cause a big bottleneck in the JavaScript thread.
+  // This includes calls from debugging libraries and logs we might forget to remove
+  plugins.push("transform-remove-console")
 }
+
+module.exports = { presets, plugins }
