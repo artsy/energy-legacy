bb63d6cda3e20738a7ee96163e26171ab9e6abb7
Mounir Dhahri <soussouheros@gmail.com>
2022-01-03 19:18:22 +0100

    use relay config for autocompletion

--- relay.config.js
+++ relay.config.js
@@ -3,5 +3,5 @@ module.exports = {
   language: "typescript",
   src: "./src",
   schema: "./data/schema.graphql",
-  exclude: ["**/node_modules/**", "**/__mocks__/**"],
+  exclude: ["**/node_modules/**", "**/__mocks__/**",  "**/__generated__/**"],
 }
